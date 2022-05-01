d315aa10da358dc350280d88243d5ce98f33721d
Revat Saharan <yashdeepsaharan@gmail.com>
2022-05-01 12:55:35 +0530

    Updates

---
+++ precache-manifest.db338bc7119d2dc53219937368190395.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6f0708716150bce7cbaface8292de39",
+    "url": "/assignment-cart/index.html"
+  },
+  {
+    "revision": "e300271ed972f8be41ec",
+    "url": "/assignment-cart/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1e94412f06a449cb8b1d",
+    "url": "/assignment-cart/static/css/main.1a5150fb.chunk.css"
+  },
+  {
+    "revision": "e300271ed972f8be41ec",
+    "url": "/assignment-cart/static/js/2.fa394f24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/assignment-cart/static/js/2.fa394f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e94412f06a449cb8b1d",
+    "url": "/assignment-cart/static/js/main.874ea744.chunk.js"
+  },
+  {
+    "revision": "4c700afb5751f1cdeaa9",
+    "url": "/assignment-cart/static/js/runtime-main.e562474c.js"
+  },
+  {
+    "revision": "fb08f8fed4141719402d79073c478131",
+    "url": "/assignment-cart/static/media/cart.fb08f8fe.svg"
+  }
+]);
